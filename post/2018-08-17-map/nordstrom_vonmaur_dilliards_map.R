818d80becdd8aab66e4877030c680c70e0cbaf21
jul-carras <Julian.Carrasquillo@chacos.com>
2018-08-20 22:17:12 -0400

    new post: web scraping, mapping

--- post/2018-08-17-map/nordstrom_vonmaur_dilliards_map.R
+++
@@ -1,8 +0,0 @@
-library(rvest)
-
-dilliards_store_finder <- read_html("https://www.dillards.com/stores")
-
-dilliards_stores <- dilliards_store_finder %>%
-  html_nodes(".col-md-5 a") %>%
-  html_text()
-
